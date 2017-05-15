e4ff230a22fb1046f3dec635ec93fe904c8fed56
paulopez78 <pau.lopez.78@gmail.com>
2017-05-15 22:04:13 +0200

    nuget package support

---
+++ publish.sh
@@ -0,0 +1,7 @@
+API_KEY=$1
+
+rm -rf ./src/EasyEventSourcing/nupkgs
+dotnet pack ./src/EasyEventSourcing/EasyEventSourcing.csproj --output nupkgs
+dotnet nuget push ./src/EasyEventSourcing/nupkgs/*.nupkg  \
+-s https://www.nuget.org/api/v2/package \
+-k $API_KEY
