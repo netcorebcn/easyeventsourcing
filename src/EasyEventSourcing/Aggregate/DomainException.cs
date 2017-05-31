f3efa39a278ff1274248f47a340c57107b30da5d
paulopez78 <pau.lopez.78@gmail.com>
2017-05-31 22:11:09 +0200

    add constructor with message

--- src/EasyEventSourcing/Aggregate/DomainException.cs
+++ src/EasyEventSourcing/Aggregate/DomainException.cs
@@ -4,5 +4,14 @@ namespace EasyEventSourcing.Aggregate
 {
     public class DomainException : Exception
     {
+        public DomainException()
+        {
+            
+        }
+
+        public DomainException(string message) : base(message)
+        {
+            
+        }
     }
 }
